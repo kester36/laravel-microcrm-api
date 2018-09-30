22a46f93d976a0750af0627809c8334ae747e416
Petro Shpyhotskyi <shpyhotskyi@gmail.com>
2018-09-30 22:45:21 +0300

    Add CRUD routes for User and Project

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
+// User CRUD
+Route::get('users', 'UserController@index');
+Route::get('users/{id}', 'UserController@show');
+Route::post('users', 'UserController@store');
+Route::put('users/{id}', 'UserController@update');
+Route::delete('users/{id}', 'UserController@delete');
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Project CRUD
+Route::get('projects', 'ProjectController@index');
+Route::get('projects/{id}', 'ProjectController@show');
+Route::post('projects', 'ProjectController@store');
+Route::put('projects/{id}', 'ProjectController@update');
+Route::delete('projects/{id}', 'ProjectController@delete');
