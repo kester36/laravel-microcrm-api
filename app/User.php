1952d554ae828c836f76c8507d36fc978809d3ff
Petro Shpyhotskyi <shpyhotskyi@gmail.com>
2018-09-30 22:01:41 +0300

    Hide password

--- app/User.php
+++ app/User.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    //
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = ['password'];
 }
