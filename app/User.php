572dbd6fbfc9185ab6d4b88fbd128d7d54103adb
Petro Shpyhotskyi <shpyhotskyi@gmail.com>
2018-10-02 21:29:49 +0300

    Add fillable fields rules

--- app/User.php
+++ app/User.php
@@ -6,10 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = ['password'];
+
+    protected $fillable = ['first_name', 'last_name', 'email'];
 }
